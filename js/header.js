9ab765f3a9605ae218dbeb145d04d048794758d7
Maximiliano Merlo <carlos.maximiliano.merlo@gmail.com>
2024-05-19 02:16:29 -0300

    agregando script a las páginas de los modulos footer y header

--- js/header.js
+++ js/header.js
@@ -1,12 +1,13 @@
 let miHeader = `
-    <div class="logo">
-        <img src="/static/imgs/logo.jpg" alt="logo-catorce" />
-    </div>
-    <nav>
-        <a href="index.html">Inicio</a>
-        <a href="templates/destinos.html">Destinos</a>
-        <a href="templates/galeria.html">Galeria</a>
-        <a href="templates/contacto.html">Contacto</a>
-    </nav>`
+        <div class="logo">
+            <img src="/static/imgs/logonuevo_1.jpeg" alt="logo-catorce" />
+
+        </div>
+        <nav>
+            <a href="/index.html">Inicio</a>
+            <a href="/templates/destinos.html">Destinos</a>
+            <a href="/templates/galería.html">Galería</a>
+            <a href="/templates/contacto.html">Contacto</a>
+        </nav>`
 
 document.querySelector("header").innerHTML = miHeader;
