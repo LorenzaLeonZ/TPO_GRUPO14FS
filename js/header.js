04244b82e9c1efd25eeb612f64a940b9fbe0eb93
Maximiliano Merlo <carlos.maximiliano.merlo@gmail.com>
2024-05-20 02:38:50 -0300

    agrego etiquetas de header y footer a index.html y modifico rutas en js f y h

--- js/header.js
+++ js/header.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     function getHeader() {
         return `
         <div class="logo">
-            <img src="./static/imgs/logonuevo_1.jpeg" alt="logo-catorce" />
+            <img src="../static/imgs/logonuevo_1.jpeg" alt="logo-catorce" />
 
         </div>
         <nav>
-            <a href="./index.html">Inicio</a>
-            <a href="./templates/destinos.html">Destinos</a>
-            <a href="./templates/galería.html">Galería</a>
-            <a href="./templates/contacto.html">Contacto</a>
+            <a href="../index.html">Inicio</a>
+            <a href="../templates/destinos.html">Destinos</a>
+            <a href="../templates/galería.html">Galería</a>
+            <a href="../templates/contacto.html">Contacto</a>
         </nav>`
     }
 
